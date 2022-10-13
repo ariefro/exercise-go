ca8f0ed3b00675863dc0b6b19b7033055fd72fab
Arief Romadhon <ariefromadhon26@gmail.com>
2022-10-13 07:59:47 +0700

    add transfer api with a custom parameter binding validator

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/ariefro/go-exercise/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 // Server serves HTTP requests
@@ -16,11 +18,18 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	// register the custom currency validator
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/api/accounts", server.createAccount)
 	router.GET("/api/account/:id", server.getAccount)
 	router.GET("/api/accounts", server.listAccount)
 	router.PUT("/api/account", server.updateAccount)
 	router.DELETE("/api/account/:id", server.deleteAccount)
+
+	router.POST("/api/transfers", server.createTransfer)
 
 	server.router = router
 	return server
