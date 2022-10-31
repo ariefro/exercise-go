fa7729c90f42751317ec89b3a8faaa8dc1ee2f7f
Arief Romadhon <ariefromadhon26@gmail.com>
2022-10-31 12:56:05 +0700

    add login user api that return paseto access token

--- api/account_test.go
+++ api/account_test.go
@@ -90,7 +90,7 @@ func TestAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(ctrl)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/api/account/%d", tc.accountId)
--- api/server.go
+++ api/server.go
@@ -1,7 +1,11 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/ariefro/go-exercise/db/sqlc"
+	"github.com/ariefro/go-exercise/middlewares"
+	"github.com/ariefro/go-exercise/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,32 +13,51 @@ import (
 
 // Server serves HTTP requests
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker middlewares.Maker
+	router     *gin.Engine
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	api := router.Group("/api")
+	{
+		api.POST("/user/register", server.createUser)
+		api.POST("/user/login", server.loginUser)
+
+		api.POST("/accounts", server.createAccount)
+		api.GET("/account/:id", server.getAccount)
+		api.GET("/accounts", server.listAccounts)
+		api.PUT("/account", server.updateAccount)
+		api.DELETE("/account/:id", server.deleteAccount)
+
+		api.POST("/transfers", server.createTransfer)
+	}
+
+	server.router = router
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := middlewares.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
 
-	// register the custom currency validator
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
-	router.POST("/api/sign-up", server.createUser)
-
-	router.POST("/api/accounts", server.createAccount)
-	router.GET("/api/account/:id", server.getAccount)
-	router.GET("/api/accounts", server.listAccount)
-	router.PUT("/api/account", server.updateAccount)
-	router.DELETE("/api/account/:id", server.deleteAccount)
-
-	router.POST("/api/transfers", server.createTransfer)
-
-	server.router = router
-	return server
+	server.setupRouter()
+	return server, nil
 }
 
 func errorResponse(err error) gin.H {
