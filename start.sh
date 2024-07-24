a1a43a4cafe4e0b7517f97c3e717ca8c3629d0b1
arief romadhon <ariefromadhon26@gmail.com>
2024-07-24 22:06:41 +0700

    change start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+# load environment variables from app.env file
+if [ -f /app/app.env ]; then
+    export $(cat /app/app.env | xargs)
+fi
+
 echo "run db migration"
-source /app/app.env
 echo $CORE_ENVIRONMENT
-if [ $CORE_ENVIRONMENT == "local" ]
+
+if [ $CORE_ENVIRONMENT = "local" ]
     then /app/migrate -path /app/migration -database "${DB_SOURCE_DEV}" -verbose up
     else /app/migrate -path /app/migration -database "${DB_SOURCE}" -verbose up
 fi
