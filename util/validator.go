5a3cc77f9876882d3fbe83b3ad65f6a65c8aba7f
arief romadhon <ariefromadhon26@gmail.com>
2024-08-09 10:27:31 +0700

    add validator for username and fullname

---
+++ util/validator.go
@@ -0,0 +1,44 @@
+package util
+
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	isValidUsername = regexp.MustCompile(`^[a-z0-9_]+$`).MatchString
+	isValidFullname = regexp.MustCompile(`^[a-zA-Z\\s]+$`).MatchString
+)
+
+func ValidateString(value string, minLength, maxLength int) error {
+	n := len(value)
+	if n < minLength || n > maxLength {
+		return fmt.Errorf("must contain from %d-%d characters", minLength, maxLength)
+	}
+
+	return nil
+}
+
+func ValidateUsername(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidFullname(value) {
+		return fmt.Errorf("must contain only lowercase letters, digits, or underscore")
+	}
+
+	return nil
+}
+
+func ValidateFullname(value string) error {
+	if err := ValidateString(value, 3, 100); err != nil {
+		return err
+	}
+
+	if !isValidUsername(value) {
+		return fmt.Errorf("must contain only letters or spaces")
+	}
+
+	return nil
+}
