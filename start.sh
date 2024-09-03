88684f66e95646e76c2a5c9788c1166dfcc70443
arief romadhon <ariefromadhon26@gmail.com>
2024-09-04 06:01:11 +0700

    change config

--- start.sh
+++ start.sh
@@ -3,8 +3,11 @@
 set -e
 
 # Load environment variables from stage.env file if it exists
-if [ -f /app/stage.env ]; then
-    export $(cat /app/stage.env | grep -v '^#' | xargs)
+if [ -f /etc/secrets/stage.env ]; then
+    echo "Loading environment variables from stage.env"
+    export $(cat /etc/secrets/stage.env | grep -v '^#' | xargs)
+else
+    echo "Warning: stage.env file not found!"
 fi
 
 echo "start the app"
